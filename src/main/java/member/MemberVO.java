4d48aec2b3a56f3449d4f7c203d7367c98f93329
JMK <jjsteel8@naver.com>
2025-05-12 12:05:42 +0900

    vo만듬

--- src/main/java/member/MemberVO.java
+++ src/main/java/member/MemberVO.java
@@ -1,5 +1,35 @@
 package member;
 
 public class MemberVO {
-
+	int user_idx;
+	String user_id;
+	String user_pw;
+	String user_nickname;
+	
+	
+	public int getUser_idx() {
+		return user_idx;
+	}
+	public void setUser_idx(int user_idx) {
+		this.user_idx = user_idx;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getUser_pw() {
+		return user_pw;
+	}
+	public void setUser_pw(String user_pw) {
+		this.user_pw = user_pw;
+	}
+	public String getUser_nickname() {
+		return user_nickname;
+	}
+	public void setUser_nickname(String user_nickname) {
+		this.user_nickname = user_nickname;
+	}
+		
 }

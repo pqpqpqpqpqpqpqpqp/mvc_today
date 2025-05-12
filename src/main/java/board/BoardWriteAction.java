a3b8a67270b647058145c194d747c68ff2173afd
DooBae-KR <tenbroker0219@gmail.com>
2025-05-12 14:31:40 +0900

    writeAction complete

--- src/main/java/board/BoardWriteAction.java
+++ src/main/java/board/BoardWriteAction.java
@@ -2,9 +2,11 @@ package board;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import controller.Action;
 import controller.ActionForward;
+import member.MemberVO;
 
 public class BoardWriteAction implements Action{
 
@@ -13,17 +15,32 @@ public class BoardWriteAction implements Action{
 		boardDAO dao = new boardDAO();
 		ActionForward forward = new ActionForward();
 		boardVO boardVO = new boardVO();
+		MemberVO memberVO = new MemberVO();
+		
+		boardVO.setBoardTitle(req.getParameter("boardTitle"));
+		boardVO.setBoardContent(req.getParameter("boardContent"));
+		boardVO.setBoardCategory(req.getParameter("boardCategory"));
+		boardVO.setTravelLocation(req.getParameter("travelLocation"));
+		boardVO.setTravelPeriod(req.getParameter("travelPeriod"));
+		boardVO.setTravelBudget(Integer.parseInt(req.getParameter("travelBudget")));
+		
+		HttpSession session = req.getSession();
+		
+		String user = null;
+		if(session.getAttribute("userId") == null) {
+			user = memberVO.getUser_id();
+		} else {
+			user = (String) session.getAttribute("userId"); 
+		}
+		boardVO.setInstUser(user);
+		boardVO.setUpdtUser(user);
+		
+		dao.boardInsert(boardVO);
 		
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		return null;
+		forward.setPath("list.bo");
+		forward.setRedirect(true);
+		return forward;
 	}
 
 }

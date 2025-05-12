ffe6e2f8d1650bc93154dfa6f6f107a905b22eef
NHD <damitup.00@gmail.com>
2025-05-12 12:16:32 +0900

    HD - merge

--- src/main/java/controller/Action.java
+++ src/main/java/controller/Action.java
@@ -1,5 +1,9 @@
 package controller;
 
-public class Action {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+public interface Action {
+
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
 }

db24064f0960725ec08d20d714699355301deac2
JMK <jjsteel8@naver.com>
2025-05-12 14:45:32 +0900

    memberRegisterAction구현완료

--- src/main/java/member/MemberRegisterAction.java
+++ src/main/java/member/MemberRegisterAction.java
@@ -3,14 +3,27 @@ package member;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import controller.Action;
 import controller.ActionForward;
 
-public class MemberRegisterAction implements Action{
-public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) throws Exception{
+public class MemberRegisterAction implements Action {
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+
+		MemberVO memberVO = new MemberVO();
+		memberVO.setUser_id(req.getParameter("userId"));
+		memberVO.setUser_pw(req.getParameter("password"));
+		memberVO.setUser_nickname(req.getParameter("nickname"));
 		
-		ActionForward forward=null;
-		
+
+		MemberDAO dao = new MemberDAO();
+		dao.regiset(memberVO);
+
+		ActionForward forward = new ActionForward();
+		forward.setPath("login.bo");
+		forward.setRedirect(true);
+
 		return forward;
+
 	}
 }

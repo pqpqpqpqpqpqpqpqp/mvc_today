8461d8b6bc31918e972adc7f56b4d0377556764d
NHD <damitup.00@gmail.com>
2025-05-12 15:30:42 +0900

    BoardLikeAction - 세션 로그인 체크

--- src/main/java/board/BoardLikeAction.java
+++ src/main/java/board/BoardLikeAction.java
@@ -1,5 +1,31 @@
 package board;
 
-public class BoardLikeAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import controller.Action;
+import controller.ActionForward;
+
+public class BoardLikeAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest req, HttpServletResponse resp) throws Exception {
+		// TODO Auto-generated method stub
+		req.setCharacterEncoding("UTF-8");
+		resp.setCharacterEncoding("UTF-8");
+		
+		// 로그인 아이디 확인, 데이터 수집 용
+		HttpSession session = req.getSession();
+		String userid = (String) session.getAttribute("userid");
+		
+		if(userid == null) {
+			ActionForward forward = new ActionForward();
+			forward.setRedirect(true);
+			forward.setPath("login.bo");
+			return forward;
+		}
+		
+	}
 
 }

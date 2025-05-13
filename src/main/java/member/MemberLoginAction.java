ebcb0dadc4b15cc2d29c5e845113609f7aae4bd5
DooBae-KR <tenbroker0219@gmail.com>
2025-05-13 11:03:52 +0900

    123

--- src/main/java/member/MemberLoginAction.java
+++ src/main/java/member/MemberLoginAction.java
@@ -22,7 +22,7 @@ public class MemberLoginAction implements Action {
 			session.setAttribute("user_id", memberVO.getUser_id());
 			session.setAttribute("user_nickname", memberVO.getUser_nickname());
 
-			forward.setPath("../scu.jsp");
+			forward.setPath(req.getContextPath()+"/board/list.bo");
 			forward.setRedirect(true);
 
 		} else {
